32bfbd9aaee86e306b5f8df4e4765e92269c69dd
alexburton7712 <116124452+alexburton7712@users.noreply.github.com>
2023-03-30 12:49:56 -0700

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,6 +1,54 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+using System.Xml;
 
-app.MapGet("/", () => "Hello World!");
+// Load the existing XML file
+XmlDocument doc = new XmlDocument();
+doc.Load("existingFile.xml");
 
-app.Run();
+// Create a new element with the correct namespace
+XmlElement newElement = doc.CreateElement("Cruise", "http://yournamespace.com/");
+
+// Create child elements with their values
+XmlElement nameElement = doc.CreateElement("Name");
+nameElement.InnerText = "Example Cruise";
+newElement.AppendChild(nameElement);
+
+XmlElement parentCompanyElement = doc.CreateElement("Parent_Company");
+parentCompanyElement.InnerText = "Example Company";
+newElement.AppendChild(parentCompanyElement);
+
+XmlElement foundedInElement = doc.CreateElement("FoundedIn");
+foundedInElement.InnerText = "2000";
+newElement.AppendChild(foundedInElement);
+
+XmlElement departurePortsElement = doc.CreateElement("Departure_Ports");
+departurePortsElement.InnerText = "Port A, Port B";
+newElement.AppendChild(departurePortsElement);
+
+XmlElement reservationElement = doc.CreateElement("Reservation");
+XmlElement phoneElement = doc.CreateElement("Phone");
+phoneElement.InnerText = "555-1234";
+reservationElement.AppendChild(phoneElement);
+XmlElement urlElement = doc.CreateElement("Url");
+urlElement.InnerText = "http://www.example.com";
+reservationElement.AppendChild(urlElement);
+newElement.AppendChild(reservationElement);
+
+XmlElement headquarterElement = doc.CreateElement("Headquarter");
+XmlElement cityElement = doc.CreateElement("City");
+cityElement.InnerText = "Example City";
+headquarterElement.AppendChild(cityElement);
+XmlElement stateElement = doc.CreateElement("State");
+stateElement.InnerText = "Example State";
+headquarterElement.AppendChild(stateElement);
+XmlElement zipElement = doc.CreateElement("Zip");
+zipElement.InnerText = "12345";
+headquarterElement.AppendChild(zipElement);
+newElement.AppendChild(headquarterElement);
+
+// Add the new element to the document's root element
+XmlElement rootElement = doc.DocumentElement;
+rootElement.AppendChild(newElement);
+
+// Save the modified document back to the file
+doc.Save("existingFile.xml");
+
