4bc3024bd0412876a78e3ef8ddd20568747fa7ff
alexburton7712 <116124452+alexburton7712@users.noreply.github.com>
2023-03-31 16:26:34 -0700

    Update VerificationService.cs

--- VerificationService.cs
+++ VerificationService.cs
@@ -6,6 +6,24 @@ using System.Web.Services;
 [WebService(Namespace = "http://yournamespace.com/")]
 public class VerificationService : WebService
 {
+
+static void Main(string[] args) {  
+    var path = new Uri(Path.GetDirectoryName(System.Reflection.Assembly.GetExecutingAssembly().CodeBase)).LocalPath;  
+    XmlSchemaSet schema = new XmlSchemaSet();  
+    schema.Add("", path + "\\input.xsd");  
+    XmlReader rd = XmlReader.Create(path + "\\input.xml");  
+    XDocument doc = XDocument.Load(rd);  
+    doc.Validate(schema, ValidationEventHandler);  
+}  
+static void ValidationEventHandler(object sender, ValidationEventArgs e) {  
+    XmlSeverityType type = XmlSeverityType.Warning;  
+    if (Enum.TryParse < XmlSeverityType > ("Error", out type)) {  
+        if (type == XmlSeverityType.Error) throw new Exception(e.Message);  
+    }  
+} 
+
+
+
     [WebMethod]
     public string Verification(string xmlUrl, string xsdUrl)
     {
